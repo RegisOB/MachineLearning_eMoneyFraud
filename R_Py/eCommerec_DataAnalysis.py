dbcd7a7a72d28ca8f4ed983f9813b4e0c6e56c0d
Regis OBIANG MBA <obiangr@ga.afro.who.int>
2020-06-28 04:03:35 +0100

    Ajout et mise à jour des fichiers

---
+++ R_Py/eCommerec_DataAnalysis.py
@@ -0,0 +1,33 @@
+
+# coding: utf-8
+
+# ## Loading libraries
+
+# In[1]:
+
+
+import pandas as pd
+import os
+
+
+# ## Setting working directory
+
+# In[2]:
+
+
+os.chdir('C:\\Users\\obiangr\\OneDrive - World Health Organization\\Documents\\Working documents\\Courses\\BootCamp Jedha\\Projet 1')
+
+
+# ## Data importation
+
+# In[5]:
+
+
+ecom.data.1 = pd.read_csv("Data\\2019-Oct.csv")
+
+
+# In[ ]:
+
+
+getcwd()
+
